381d09df97925f7f262f7aa6a205339d733320da
tony baldwin <tony@tonybaldwin.org>
2014-07-12 20:25:01 -0400

    control diaspora xposting

--- reddi.sh
+++ reddi.sh
@@ -29,10 +29,14 @@ read -p "which channel? " chan
 read -p "Will we be crossposting?  (y/n) " xpo
 if [[ $xpo == "y" ]]; then
      # crosspost to friendica?
-     read -p "xpost to friendica? (y/n) " fxq
+          read -p "xpost to friendica? (y/n) " fxq
      if [[ $fxq == "y" ]]; then
      	fx=1
      fi
+     read -p "xpost to diaspora? (y/n) " dia
+     if [[ $dia = "y" ]]; then
+	     dp=1
+     fi 
      read -p "xpost to pumpio? (y/n) " ppx
      if [[ $ppx == "y" ]]; then
      	pp=1
@@ -62,12 +66,12 @@ if [[ $xpo == "y" ]]; then
      	sn=1
      fi
 else	
-     let fx=pp=wp=ij=lj=dw=lt=sn=0
+     let fx=pp=wp=ij=lj=dw=lt=sn=dp=0
 fi
 
 # and this is the curl command that sends the update to the server
  
-if [[ $(curl --ssl -u $user:$pass -d "status=$ud&title=$title&channel=$chan&rtof_enable=$fx&pumpio_enable=$pp&ljpost_enable=$lj&ijpost_enable=$ij&dwpost_enable=$dw&wppost_enable=$wp&libertree_enable=$lt&statusnet_enable=$sn&app=reddi.sh" $rurl/api/statuses/update.xml | grep error) ]]; then
+if [[ $(curl --ssl -u $user:$pass -d "status=$ud&title=$title&channel=$chan&rtof_enable=$fx&pumpio_enable=$pp&ljpost_enable=$lj&ijpost_enable=$ij&dwpost_enable=$dw&wppost_enable=$wp&libertree_enable=$lt&diaspora_enable=$dp&statusnet_enable=$sn&app=reddi.sh" $rurl/api/statuses/update.xml | grep error) ]]; then
  
 # what does the server say?
  
